7bb19fc73cdb34db69c3f93b2261c650da428bda
Derlino <sanderzandi@gmail.com>
2018-04-11 13:06:46 +0200

    changed some stuff

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 
-
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -10,10 +9,9 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(selfself):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
-
 
     def __str__(self):
         return self.title
